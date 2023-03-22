d3b8ccb641798ab793bc2416a76a9bb012b355d9
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-22 20:33:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const ButtonCounter=()=>{
-  const [count, setCount] = useState(0);
+const ButtonCounter = () => {
+  const [ count, setCount ] = useState(0);
 
   const handleClick = () => {
     setCount(count + 1);
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>Click Me</button>
-      <p>Button clicked {count} times</p>
-    </div>
+    <>
+      <button onClick={handleClick}>Button</button>
+      <p>Button clicked {count} times.</p>
+    </>
   );
-}
+};
 
 export default ButtonCounter;
